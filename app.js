10fcc4424636c437605a9119bad5647521ccd040
facuuzerbi <facuzerbinatti@gmail.com>
2020-12-12 18:48:51 -0300

    LogIn y SigIn completos

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+//app.use(express.static(‘public’));
+
+app.listen(3030, ()=> console.log('Servidor Corriendo')); 
+
+app.get('/login', (req,res) => {
+    let htmlPath = path.resolve(__dirname,'./views/Login.html');
+    res.sendFile(htmlPath);
+});
+
+app.get('/sigin', (req,res) => {
+    let htmlPath = path.resolve(__dirname,'./views/Sigin.html');
+    res.sendFile(htmlPath);
+});
+
