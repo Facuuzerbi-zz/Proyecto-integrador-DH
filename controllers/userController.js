4cff2a9dcd2e632d925bd8356c9f32eec4cd758b
nataliaquiroga <natalia.b.quiroga@gmail.com>
2021-05-05 21:42:27 -0300

    modificaciones controlador de user y validacion de campo requerido en vista login

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,109 +3,81 @@ const { validationResult } = require('express-validator');
 const db = require('../database/models');
 const User = db.User;
 
-const userController ={
+const userController = {
     signin: (req,res) => {
         return res.render('../views/users/signin');
     },
-    processSignin:(req,res)=>{
-        const resultValidation = validationResult(req);
-
-        if (resultValidation.errors.length > 0) {
-            return res.render ('../views/users/signin', {
-                errors: resultValidation.mapped(),
-                oldData: req.body
-            });
-        }
-        
-        let userInDB =  db.users.findAll({attributes: ['email'], where: {email:req.body.email} })
-
-
-        if (userInDB =!"<pending>"){
-            return res.render ('../views/users/signin.ejs', {
-                errors: {
-                    email: {
-                        msg: 'Este email ya está registrado'
-                    }
-                },
-                oldData: req.body
-            });
-        }
-            db.users.create({
+     processSignin: async (req,res)=>{
+        try {
+            const password =req.body.password;
+            const hash = await bcryptjs.hash(password,10);
+            await db.users.create({
                 firstname:req.body.first_name,
                 lastname:req.body.last_name,
                 email:req.body.email,
-                password:req.body.password,
+                password:hash,
             });
-            let userToCreate = {
-                ...req.body,
-                password: bcryptjs.hashSync(req.body.password, 10)
-            }
-             res.redirect('../user/login');
-        },
-       
+            console.log('todo ok');
+            res.redirect('../user/login');
+
+
+        } catch(e){
+            console.log(e);
+            console.log('algo anda mal');
+        }
+        
+     
+    },
     login: (req,res) => {
 
-            return res.render('../views/users/login.ejs');
-        
-    },
-
-    processLogin: (req,res) => {
-        
-        let email = [req.body.email];
-        let password = [req.body.password];     
-
-
-        let userToLogin =  db.users.findOne({
-            attributes: [
-                "email",
-                "password"
-                ],
-        where: { email: email[0],
-        
-       }
-
-    })
-    .then(function(userToLogin){
-        
-        console.log("hola " + userToLogin.password + "holaaaa " + password)
-       
-    })
-        if(userToLogin) {
-            let PassOk =  bcryptjs.compare(password,userToLogin.password) 
-            
-            .then(function(PassOk){
-                console.log(PassOk)
-            })
-            
-            if(PassOk) {
-                
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
-
-                if(req.body.remember_user){
+        return res.render('../views/users/login.ejs');
+    
+},
+    processLogin: async (req,res) => {
+        try {                
+            let password = req.body.password
+            const user = await db.users.findOne({
+                attributes: [
+                    "email", "password"],
+            where: {email: req.body.email}
+    
+        });
+            if(user){
+                const validPass = await bcryptjs.compare(password, user.hash);
+                if(validPass){
+                    console.log('pass correcta');
+                    delete user.password;
+                    req.session.userLogged = user;
+                    
+                    if(req.body.remember_user){
                     res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 });
                 }
 
-                return res.redirect('/user/profile');
-            } 
-
+                    return res.redirect('/user/profile');
+                } else {
+                    console.log('pass incorrecta');
+                }
+            } else {
+                console.log('user no encontrado');
+                return res.render('../views/users/login.ejs', {
+                    errors: {
+                        email: {
+                            msg: 'Este email no se encuentra registrado'
+                        }
+                    }
+                });
+            }
+        } catch (e){
+            console.log(e);
+            console.log('algo se rompio')
             return res.render('../views/users/login.ejs', {
                 errors: {
                     email: {
-                        msg: 'Las credenciales son inválidas'
+                        msg: 'Problema en Credenciales'
                     }
                 }
             });
-        }
-    
-        return res.render('../views/users/login.ejs', {
-            errors: {
-                email: {
-                    msg: 'Este email no se encuentra registrado'
-                }
-            }
-        });
-    
+        }         
     },
 
     profile: (req,res) => {
